cb15ce8ec8397ea5da46ffacf33677ecaf49ca6e
CarolinaRuescas <ruescas17@gmail.com>
2025-10-06 10:32:03 +0200

    comienzo

--- Tarea3/ejercicio1.js
+++ Tarea3/ejercicio1.js
@@ -2,3 +2,23 @@
 1.Se le pide al usuario el número de valores a introducir. 
 Posteriormente se le van pidiendo.Se le devuelven ordenados. ¿Cómo?
 */
+
+var cantidad = Number(prompt("¿Cúnatos números quieres introducir?"));
+
+let valores = [];
+let i = 0;
+
+while (i < cantidad) {
+  let valor = parseFloat(prompt("Introduce el valor #" + (i + 1)));
+  
+  if (!isNaN(valor)) {
+    valores.push(valor);
+    i++;
+  } else {
+    alert("Eso no es un número. Intenta otra vez.");
+  }
+}
+
+valores.sort(function(a, b) { return a - b; });
+
+alert("Valores ordenados: " + valores.join(", "));
--- Tarea3/ejercicio3.js
+++ Tarea3/ejercicio3.js
@@ -1,3 +1,32 @@
 /*
 3.Crea una lista de notas. Calcula la media total y la media sólo de los aprobados. BONUS si se hace con y sin arrays.
 */
+
+var notas = [9,5,6,7,8,4 ,3 ];
+let suma = 0;
+let sumaAprobados = 0;
+let sumaSuspensos = 0;
+let countAprobados = 0;
+let countSuspensos = 0;
+
+
+
+
+for(i = 0; i < notas.length ; i++){
+    suma += notas [i];
+    if  (notas[i] < 5){
+    sumaSuspensos += notas[i];
+    countSuspensos++;
+
+    }else{
+    sumaAprobados += notas[i];
+    countAprobados++;
+
+    }
+    
+}
+let media = suma/notas.length;
+let mediaAprobados = sumaAprobados /countAprobados
+let mediaSuspensos= sumaSuspensos /countSuspensos
+
+alert("La media total de los aprobados es " + mediaAprobados + ". La media total de los suspensos es " + mediaSuspensos);
